f895412286154438ae69837dff9cd2de2f195008
kvist49 <quist-ch@hotmail.com>
2015-01-22 22:22:24 +0100

    Raycast
    Har lavet raycast så den tjekker hvilken der er ovenfor

--- Assets/Scripts/MouseHover.cs
+++ Assets/Scripts/MouseHover.cs
@@ -2,16 +2,49 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class MouseHover : MonoBehaviour {
+public class MouseHover : MonoBehaviour 
+
+{
 
 	List<int> crosses = new List<int>();
 	public bool IsOccupied;
+
+	public bool IsCross;
+	public bool IsCircle;
+
 	public Texture2D textureCross;
     public Texture2D textureCircle;
+
+
 
 	void Start () {
 		renderer.material.color = new Color (15.0F, 0, 0);
 		IsOccupied = false;
+	}
+
+	void Update ()
+	{
+		RaycastHit hit;
+
+		Ray ray = new Ray(transform.position, transform.up * 10);
+		Debug.DrawRay (transform.position, transform.up * 10, Color.green);
+
+		if(Physics.Raycast(ray, out hit))
+		   {
+			if(hit.collider.gameObject.tag == "Cross")
+			{
+				IsCross = true;
+				IsCircle = false;
+
+			}
+
+			if(hit.collider.gameObject.tag == "Circle")
+			{
+				IsCross = false;
+				IsCircle = true;
+			}
+		}
+
 	}
 
 	void OnMouseEnter() {
@@ -28,12 +61,14 @@ public class MouseHover : MonoBehaviour {
         {
             if (MainScript.CrossCricle == 0) {
                 renderer.material.mainTexture = textureCross;
+				gameObject.tag = "Cross";
                 IsOccupied = true;
                 MainScript.CrossCricle++;
             }
             else if (MainScript.CrossCricle == 1)
             {
                 renderer.material.mainTexture = textureCircle;
+				gameObject.tag = "Circle";
                 IsOccupied = true;
                 MainScript.CrossCricle--;
             }
